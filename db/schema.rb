959a445ebc39da64cb1abf9f85c342e205a814e9
Jeff Q <jqiu1998@yahoo.com>
2014-07-31 20:01:43 -0400

    New Comment System

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140724010014) do
+ActiveRecord::Schema.define(version: 20140730232758) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
